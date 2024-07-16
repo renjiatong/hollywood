2c99fb156d25967e152fa75e055ef5d9b672e149
renjiatong <renjiatongjanet@gmail.com>
2024-07-16 16:02:34 +0800

    init

---
+++ src/main/java/com/jiatong/hollywood/HollywoodApplication.java
@@ -0,0 +1,13 @@
+package com.jiatong.hollywood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HollywoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HollywoodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiatong/hollywood/HollywoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiatong.hollywood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HollywoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
